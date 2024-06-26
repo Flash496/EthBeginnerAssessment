db77f720640d636ff9b2710c3b7fceaf32d63915
Debangshu <121332582+Flash496@users.noreply.github.com>
2024-06-26 15:42:31 +0530

    Update token.sol

--- Eth+Avax_tokenContract/token.sol
+++ Eth+Avax_tokenContract/token.sol
@@ -1,1 +1,23 @@
-bshs
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract FlashToken is ERC20, Ownable {
+    uint256 public constant MAX_SUPPLY = 1000000 * 10**18; // 1 million tokens
+    
+    constructor() ERC20("FlashToken", "Flash") Ownable(msg.sender) {
+        // Mint 100,000 tokens to the contract creator upon deployment
+        _mint(msg.sender, 100000 * 10**18);
+    }
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        require(totalSupply() + amount <= MAX_SUPPLY, "Exceeds maximum supply");
+        _mint(to, amount);
+    }
+
+    function burn(uint256 amount) public {
+        _burn(msg.sender, amount);
+    }
+}
