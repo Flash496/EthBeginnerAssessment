b681723ee86d2888ba663f3b1623442cd5b43c40
Debangshu Baul <121332582+Flash496@users.noreply.github.com>
2024-06-29 22:54:26 +0530

    Update token.sol

--- Eth+Avax_tokenContract/token.sol
+++ Eth+Avax_tokenContract/token.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "hardhat/console.sol";
 
 contract FlashToken is ERC20, Ownable {
     uint256 public constant MAX_SUPPLY = 1000000 * 10**18; // 1 million tokens
@@ -19,5 +20,14 @@ contract FlashToken is ERC20, Ownable {
 
     function burn(uint256 amount) public {
         _burn(msg.sender, amount);
+    }
+
+    //Explicitly added and overriden transfer function
+    function transfer(address to, uint256 amount) override public virtual returns (bool) {
+        bool transferred = super.transfer(to, amount);          //super keyword keeps the original transfer function from the parent contract (ERC20)
+        if (transferred) {
+            console.log("Transferred %s tokens from %s to %s", amount, msg.sender, to);
+        }
+        return transferred;
     }
 }
